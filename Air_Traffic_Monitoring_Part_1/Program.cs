bf5205a87dbfa49836489eb0f13aa235b9d9fa77
Oliver Monberg <au554208@uni.au.dk>
2018-09-25 08:30:20 +0200

    Add project files.

---
+++ Air_Traffic_Monitoring_Part_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Air_Traffic_Monitoring_Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NOOB
+        }
+    }
+}
